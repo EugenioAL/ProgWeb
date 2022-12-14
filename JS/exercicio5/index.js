4b94a39acdc00fae7db9262021868176a0436c46
Eugênio <eaof@icomp.ufam.edu.br>
2022-12-14 12:51:39 -0300

    exercico 5 e 7 de JS adicionados

---
+++ JS/exercicio5/index.js
@@ -0,0 +1,12 @@
+function areaCirculo(num){
+    return num*num*Math.PI;
+}
+
+function compCirculo(num){
+    return 2*Math.PI.num;
+}
+
+
+document.getElementById("asdf").onclick = function(){
+    console.log("boa sorte");
+}
