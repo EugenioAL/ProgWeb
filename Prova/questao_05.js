df97116a5d74937ea557ad604d25b43c9d7aff43
eugenioal <eaof@icomp.ufam.edu.br>
2022-12-14 17:10:43 -0400

    prova feita

---
+++ Prova/questao_05.js
@@ -0,0 +1,44 @@
+class Venda{
+    constructor(id,quant,preco){
+        this.id = id;
+        this.quant = quant;
+        this.preco = preco;
+    }
+
+    setId(val){
+        this.id = val;
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    setQuant(val){
+        this.id = this.quant;
+    }
+
+    getQuant(){
+        return this.quant;
+    }
+
+    setPreco(val){
+        this.id = val;
+    }
+
+    getPreco(){
+        return this.id;
+    }
+
+    getValorTotal(){
+        return this.quant * this.preco;
+    }
+}
+
+const venda1 = new Venda(12,10,3);
+
+console.log(venda1.getValorTotal());
+
+const venda2 = new Venda(9,7,2);
+
+console.log(venda2.getValorTotal());
+
